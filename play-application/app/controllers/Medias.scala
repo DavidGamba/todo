65fcb99feeb2e8267f17ef5cb3c7817abfa15c0d
David Gamba <david.gamba@calgaryscientific.com>
2014-01-02 15:02:20 -0700

    Initial implementation on how to play one audio file after another
    When playing several audio files together, you can't go to a
    previous point in time regardless of the content-length.
    
    After playing with this for a while and exausting (I think) how far it
    goes I decided that I will go the js playlist way to play one file after
    another, seems easier and it doesn't have issue I have here.

--- play-application/app/controllers/Medias.scala
+++ play-application/app/controllers/Medias.scala
@@ -12,7 +12,12 @@ object Medias extends Controller {
   // TODO: Make errors i18n
 
   def song(name: String) = {
-    get_file( Media.get_song_filename(name) )
+    val filename = Media.get_song_filename(name)
+    val file = new java.io.File(filename)
+    if (file.isDirectory)
+      get_files(file.listFiles)
+    else
+      get_file(filename)
   }
 
   def video(name: String) = {
@@ -41,4 +46,35 @@ object Medias extends Controller {
         BadRequest("Could not find file. Error: " + e.getMessage)
     }
   }
+
+  // TODO: when playing several audio files together, you can't go to a
+  // previous point in time regardless of the content-length.
+  def get_files(files: Array[java.io.File]) = Action { implicit request =>
+    import java.lang.IllegalArgumentException
+    import java.io.FileNotFoundException
+    import scala.collection.JavaConverters._
+    try {
+      val length_array = for (file <- files) yield file.length
+      val length = length_array.sum + 100
+
+      val inputStream_array = for (file <- files) yield
+        new java.io.FileInputStream(file)
+
+      val inputStream_iterator = inputStream_array.toIterator
+
+      val sequence = new java.io.SequenceInputStream(inputStream_iterator.asJavaEnumeration)
+      val fileContent: Enumerator[Array[Byte]] = Enumerator.fromStream(sequence)
+      SimpleResult(
+        header = ResponseHeader(200, Map(CONTENT_LENGTH -> length.toString)),
+        body = fileContent
+      )
+    }
+    catch {
+      case e: IllegalArgumentException =>
+        BadRequest("Could not retrieve file. Error: " + e.getMessage)
+      case e: FileNotFoundException =>
+        BadRequest("Could not find file. Error: " + e.getMessage)
+    }
+  }
+
 }
